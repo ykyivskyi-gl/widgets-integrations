ff9c06fb4935c871f0e03f9b19b6444634120b8e
Yevhen Kyivskyi <yevhen.kyivskyi@glia.com>
2024-09-13 16:38:36 +0200

    Initial Commit

---
+++ WidgetsIntegration/WidgetsIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsIntegrationApp.swift
+//  WidgetsIntegration
+//
+//  Created by Yevhen Kyivskyi on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
